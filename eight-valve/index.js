fac6392498045a87f83b8e0aa0bad4ce26d4ff7e
brentkirkland <brentckirkland@gmail.com>
2017-07-06 23:41:13 -0700

    more data

--- eight-valve/index.js
+++ eight-valve/index.js
@@ -1,7 +1,13 @@
 var Bleacon = require('bleacon');
 var fetch = require('node-fetch');
 
+//gotEight helps prevent race condition of sending multiple messages
+var gotEight = false;
+
+//should text tells twilio to send message. False is helpful for debug
 var shouldText = false;
+
+// uncomment the following line to text not on the hour
 Bleacon.startScanning();
 setInterval(function() {
   var d = new Date()
@@ -15,23 +21,32 @@ setInterval(function() {
   }
 }, 60000)
 
+
+// TODO: Make this one array
 var major = [];
 var minor = [];
+var bleacon_data = [];
 
 Bleacon.on('discover', function(bleacon) {
 
   var bleaconMajorHex = bleacon.major.toString(16);
   var bleaconMinorHex = bleacon.minor.toString(16);
-  console.log(bleacon)
 
   if (major.includes(bleaconMajorHex)) {
-    if (major.length === 8) {
+    if (major.length === 8 && !gotEight) {
+      gotEight = true;
+
+      // stop the scanning!
       Bleacon.stopScanning();
+
+      // run through the collected data of all eight devices
       var avgTemp = 0;
       var readings = [];
       for (var i = 0; i < major.length; i++) {
         var temp;
         var moisture;
+
+        // get the correct temperature and moisture
         if (minor[i].length === 4) {
           temp = minor[i][2] + minor[i][3];
           moisture = minor[i][0] + minor[i][1];
@@ -39,19 +54,32 @@ Bleacon.on('discover', function(bleacon) {
           temp = minor[i][1] + minor[i][2];
           moisture = minor[i][0];
         }
+
+        // make the US happy
         var fahren = parseInt(temp, 16) * 9 / 5 + 32;
         avgTemp += fahren;
 
-
+        // Major is reveresed from device, switch it around so it doesn't confuse others
+        var switched_major;
+        if (major[i].length === 4) {
+          switched_major = minor[i][2] + minor[i][3] + minor[i][0] + minor[i][1]
+        } else if (major[i].length === 3) {
+          switched_major = '0' + minor[i][2] + minor[i][0] + minor[i][1]
+        }
         var data = {
           minor: minor[i],
-          major: major[i],
+          major: switched_major,
           celcius: parseInt(temp, 16),
           fahrenheit: fahren,
           moisture: parseInt(moisture, 16),
           lastWatered: '--',
           watered: false,
-          device_nickname: major[i]
+          device_nickname: major[i],
+          uuid: bleacon_data[i].uuid,
+          measuredPower: bleacon_data[i].measuredPower,
+          rssi: bleacon_data[i].rssi,
+          accuracy: bleacon_data[i].accuracy,
+          proximity: bleacon_data[i].proximity
         }
         readings.push(data)
       }
@@ -84,6 +112,7 @@ Bleacon.on('discover', function(bleacon) {
 
       major = [];
       minor = [];
+      bleacon_data = [];
 
       // TODO: Better error handling
       fetch('https://us-central1-slurp-165217.cloudfunctions.net/pubEndpoint?topic=processMeasures', data)
@@ -92,6 +121,7 @@ Bleacon.on('discover', function(bleacon) {
   } else {
     major.push(bleaconMajorHex);
     minor.push(bleaconMinorHex);
+    bleacon_data.push(bleacon)
   }
 
 });
